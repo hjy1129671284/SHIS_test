d5c6b9be3b42bf53b2f3af26eb54ff5403876818
hjy1129671284 <m18934586031_1@163.com>
2021-09-17 11:56:19 +0800

    Initial commit

---
+++ SHIS_test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SHIS_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
